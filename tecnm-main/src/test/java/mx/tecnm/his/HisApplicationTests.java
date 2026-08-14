d683212b211e89ce9f07e7e9728ae76983774c1e
Raul67r <s21120219@alumnos.itsur.edu.mx>
2026-08-14 07:18:31 -0600

    primer comit

---
+++ tecnm-main/src/test/java/mx/tecnm/his/HisApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.tecnm.his;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
